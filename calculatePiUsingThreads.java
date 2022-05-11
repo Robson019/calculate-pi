51fe84d7acb05c0ceb3a5f00119d1858db96374e
Robson Alves <Reep55555@gmail.com>
2022-05-11 00:02:49 +0000

    calcular pi com uma thread

--- calculatePiUsingThreads.java
+++ calculatePiUsingThreads.java
@@ -2,12 +2,12 @@ public class calculatePiUsingThreads {
     public static void main(String[] args) {
         int n = 1000000;
         double sum = 0;
-        for (int i = 0; i < n; n++) {
-            double portion = Math.pow(-1, i)/(2*i + 1);
+        for (int i = 0; i < n; i++) {
+            double portion = Math.pow(-1, (double) i)/(2*i + 1);
             sum += portion;
         }
         double pi = sum * 4;
 
-        System.out.println(pi);
+        System.out.println("Valor: " + pi);
     }
 }
